3f4d5bd1e0325b475076fe8c8984a3c464b48aff
eshan36 <125075911+eshan36@users.noreply.github.com>
2023-09-14 23:11:19 +0600

    Update image_process.m

--- others/image_process.m
+++ others/image_process.m
@@ -1,22 +1,29 @@
-function pivoting(k,a,b)
-p=k;n=size(a,2);
-big=a(k,k);
-for i=k+1:n
-    if(abs(a(i,k))>abs(big))
-        big=a(i,k);
-        p=i;
-    end
+%subtracting background and finding position of flurosence
+ds1=imageDatastore("D:\L3T1\Assignment Folder\Background Image\5 atm_gain 80_C001H001S0001");
+ds2=imageDatastore("D:\L3T1\Assignment Folder\TwoPhoton Fluorescence Image\5 atm_gain 50_C001H001S0001_Flame");
+nfiles1=numel(ds1.Files);
+nfiles2=numel(ds2.Files);
+s1=double(zeros(1024));
+for i=1:nfiles1
+    img=readimage(ds1,i);
+    s1=s1+double(img);
 end
-if (p~=k)
-    for j=k:n
-        dum=a(p,j);
-        a(p,j)=a(k,j);
-        a(k,j)=dum;
-    end
-    dum=b(p);
-    b(p)=b(k);
-    b(k)=dum;
+finalbg=s1/nfiles1;
+s2=double(zeros(1024));
+for i=1:nfiles2
+    img=readimage(ds2,i);
+    s2=s2+double(img);
 end
-disp(a);
-disp(b);
-end
+final2phoflo=s2/nfiles2;
+Subtractedbg=final2phoflo-finalbg;
+rescaleim=rescale(Subtractedbg,0,255);
+signal=uint8(rescaleim);
+finalimage=imadjust(signal);
+imshow(finalimage);
+x_sum=sum(finalimage,1);
+y_sum=sum(finalimage,2);
+x_min=min(x_sum);
+y_min=min(y_sum);
+x=find(x_sum==x_min);
+y=find(y_sum==y_min);
+fprintf('position of flurosence signal is %d,%d',x,y);
